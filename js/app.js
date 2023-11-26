26c7b4a85aa47513e810e8f87a8b348d49ddf4f6
ArbiterGM <123399265+ArbiterGM@users.noreply.github.com>
2023-11-26 23:50:35 +0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 
+const addBtn = document.querySelector("#add-btn");
+
 const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
@@ -12,23 +14,23 @@ let LIST, id;
 let data = localStorage.getItem("TODO");
 
 if (data) {
-    LIST = JSON.parse(data);
-    id = LIST.length;
-    loadList(LIST);
+  LIST = JSON.parse(data);
+  id = LIST.length;
+  loadList(LIST);
 } else {
-    LIST = [];
-    id = 0;
+  LIST = [];
+  id = 0;
 }
 
 function loadList(array) {
-    array.forEach(function (item) {
-        addToDo(item.name, item.id, item.done, item.trash);
-    });
+  array.forEach(function (item) {
+    addToDo(item.name, item.id, item.done, item.trash);
+  });
 }
 
 clear.addEventListener("click", function () {
-    localStorage.clear();
-    location.reload();
+  localStorage.clear();
+  location.reload();
 });
 
 const options = { weekday: "long", month: "short", day: "numeric" };
@@ -36,73 +38,78 @@ const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
-
 function addToDo(toDo, id, done, trash) {
+  if (trash) {
+    return;
+  }
 
-    if (trash) { return; }
+  const DONE = done ? CHECK : UNCHECK;
+  const LINE = done ? LINE_THROUGH : "";
 
-    const DONE = done ? CHECK : UNCHECK;
-    const LINE = done ? LINE_THROUGH : "";
-
-    const item = `<li class="item">
+  const item = `<li class="item">
                     <i class="fa ${DONE} co" job="complete" id="${id}"></i>
                     <p class="text ${LINE}">${toDo}</p>
                     <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
                   </li>
                 `;
 
-    const position = "beforeend";
+  const position = "beforeend";
 
-    list.insertAdjacentHTML(position, item);
+  list.insertAdjacentHTML(position, item);
 }
 
-document.addEventListener("keyup", function (even) {
-    if (event.keyCode == 13) {
-        const toDo = input.value;
+function addToDoFn() {
+  const toDo = input.value;
+  addToDo(toDo, id, false, false);
 
-        if (toDo) {
-            addToDo(toDo, id, false, false);
+  LIST.push({
+    name: toDo,
+    id: id,
+    done: false,
+    trash: false,
+  });
 
-            LIST.push({
-                name: toDo,
-                id: id,
-                done: false,
-                trash: false
-            });
+  localStorage.setItem("TODO", JSON.stringify(LIST));
 
-            localStorage.setItem("TODO", JSON.stringify(LIST));
+  id++;
+  input.value = "";
+}
 
-            id++;
-        }
-        input.value = "";
+document.addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    const toDo = input.value;
+
+    if (toDo) {
+      addToDoFn();
     }
+  }
 });
 
-
 function completeToDo(element) {
-    element.classList.toggle(CHECK);
-    element.classList.toggle(UNCHECK);
-    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
 
-    LIST[element.id].done = LIST[element.id].done ? false : true;
+  LIST[element.id].done = LIST[element.id].done ? false : true;
 }
 
 function removeToDo(element) {
-    element.parentNode.parentNode.removeChild(element.parentNode);
+  element.parentNode.parentNode.removeChild(element.parentNode);
 
-    LIST[element.id].trash = true;
+  LIST[element.id].trash = true;
 }
 
-
 list.addEventListener("click", function (event) {
-    const element = event.target;
-    const elementJob = element.attributes.job.value;
+  const element = event.target;
+  const elementJob = element.attributes.job.value;
 
-    if (elementJob == "complete") {
-        completeToDo(element);
-    } else if (elementJob == "delete") {
-        removeToDo(element);
-    }
+  if (elementJob == "complete") {
+    completeToDo(element);
+  } else if (elementJob == "delete") {
+    removeToDo(element);
+  }
 
-    localStorage.setItem("TODO", JSON.stringify(LIST));
+  localStorage.setItem("TODO", JSON.stringify(LIST));
 });
+
+addBtn.addEventListener("click", addToDoFn);
